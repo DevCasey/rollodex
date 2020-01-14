a6733558c7c03ada218b82d253cc19480896e2da
Casey Calkins <c.angelcalkins@outlook.com>
2020-01-13 20:55:19 -0600

    User name, picture and details button displaying. Details button is not doing anything yet

--- src/App.js
+++ src/App.js
@@ -16,18 +16,19 @@ class App extends Component {
   fetchUsers = () => {
     fetch('https://randomuser.me/api?results=25')
     .then((res) => { return res.json() })
-    .then((res) => { console.log(res);this.setState( {users: res.results}) })
-    // .then((res) => {console.log(this.state.users)})
-    // .catch(error => console.log('Parsing failed', error))
+    .then((res) => { console.log(res); this.setState( {users: res.results}) })
+    .catch(error => console.log('Parsing failed', error))
   }
 
 
   render () {
     return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
+      <div>
+        {this.state.users.map((item,index) => {
+        return <div key={index}>{<img src={item.picture.medium}/>} {item.name.first} {item.name.last}<button>Details</button></div>
+        })}
+      </div>
     </div>
     )
   } 
