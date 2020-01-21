a06f864cbefe9cf9e25632bd4a586352b03632ed
Casey Calkins <c.angelcalkins@outlook.com>
2020-01-20 19:25:40 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state= {
-      users: []
+      users: [],
+      details: [],
+      isToggleOn: false,
     }
   }
 
   componentDidMount() {
     this.fetchUsers();
   }
+
+  toggleDetails() {
+    let getButtonId = document.getElementById('details-button');
+    getButtonId.textContent = "Hide Details";
+  }
+
+
 
   fetchUsers = () => {
     fetch('https://randomuser.me/api?results=25')
@@ -26,8 +35,16 @@ class App extends Component {
     <div className="App">
       <div>
         {this.state.users.map((item,index) => {
-        return <div key={index}>{<img src={item.picture.medium}/>} {item.name.first} {item.name.last}<button>Details</button></div>
-        })}
+        return (
+        <div key={index}>
+          {<img src={item.picture.medium}/>}
+          <div>
+          {item.name.first} {item.name.last}
+          </div>    
+            <button id="details-button" onClick={this.toggleDetails}>Details</button>
+        </div>
+        )
+       })}
       </div>
     </div>
     )
